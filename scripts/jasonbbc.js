f95de8789048fba8e383bfd356770b036e8d5bda
Phoenixsong <phoenixkindred@gmail.com>
2014-03-02 19:53:37 -0500

    Core lex functionality

--- scripts/jasonbbc.js
+++ scripts/jasonbbc.js
@@ -1,12 +1,26 @@
 var VERBOSE = true;
 var INPUT = "";
+var TOKENS = [];
 
 function init(){
   var editor = ace.edit("editor");
   editor.getSession().setMode("ace/mode/c_cpp");
   $("#compile").on("click", function(){
     INPUT = editor.getValue();
-    lex();
+    var steps = ["lex"];
+    clearOutput();
+    output("Starting compilation");
+    // call each step of the compilation process and terminate if one fails
+    for (var i = 0; i < steps.length; i++){
+      output("Starting " + steps[i]);
+      if (window[steps[i]]()){
+        output("Successfully completed {0}".format(steps[i]));
+      }
+      else{
+        output("Failed on ".format(steps[i]));
+        return;
+      }
+    }
   });
   $("#verbose").on("click", function(){
     if (VERBOSE){
@@ -18,4 +32,24 @@ function init(){
       VERBOSE = true;
     }
   });
+}
+
+function output(s){
+  $("#output").append(s + "<br />\n");
+}
+
+function clearOutput(){
+  $("#output").text("");
+}
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
 }
--- scripts/lexer.js
+++ scripts/lexer.js
@@ -1,11 +1,11 @@
 var tokenMatrix =
-[ {"a": 1, "b": 3, "c": 1, "d": 1, "e": 1, "f": 4, "g": 1, "h": 1, "i": 5, "j": 1, "k": 1, "l": 1, "m": 1, "n": 1, "o": 1, "p": 1, "q": 1, "r": 1, "s": 6, "t": 7, "u": 1, "v": 1, "w": 8, "x": 1, "y": 1, "z": 1, "1": 2, "2": 2, "3": 2, "4": 2, "5": 2, "6": 2, "7": 2, "8": 2, "9": 2, "0": 2, "$": 9, "{": 10, "}": 11, "(": 12, ")": 13, "=": 14, "\"": 15, "!": 17, "+": 18}, // 0
+[ {"a": 1, "b": 3, "c": 1, "d": 1, "e": 1, "f": 4, "g": 1, "h": 1, "i": 5, "j": 1, "k": 1, "l": 1, "m": 1, "n": 1, "o": 1, "p": 25, "q": 1, "r": 1, "s": 6, "t": 7, "u": 1, "v": 1, "w": 8, "x": 1, "y": 1, "z": 1, "1": 2, "2": 2, "3": 2, "4": 2, "5": 2, "6": 2, "7": 2, "8": 2, "9": 2, "0": 2, "$": 9, "{": 10, "}": 11, "(": 12, ")": 13, "=": 14, "\"": 15, "!": 17, "+": 18}, // 0
   {"accept": "T_id"}, // 1
   {"accept": "T_digit", "1": 2, "2": 2, "3": 2, "4": 2, "5": 2, "6": 2, "7": 2, "8": 2, "9": 2, "0": 2}, // 2
   {"accept": "T_id", "o": 19}, // 3
-  {"accept": "T_id", "a": 25}, // 4
+  {"accept": "T_id", "a": 26}, // 4
   {"accept": "T_id", "f": 30, "n": 31}, // 5
-  {"accept": "T_id", "t": 33}, // 6
+  {"accept": "T_id", "t": 34}, // 6
   {"accept": "T_id", "r": 39}, // 7
   {"accept": "T_id", "h": 42}, // 8
   {"accept": "T_EOF"}, // 9
@@ -14,7 +14,7 @@ var tokenMatrix =
   {"accept": "T_LParen"}, // 12
   {"accept": "T_RParen"}, // 13
   {"accept": "T_assignment", "=": 46}, // 14
-  {"a": 15, "b": 15, "c": 15, "d": 15, "e": 15, "f": 15, "g": 15, "h": 15, "i": 15, "j": 15, "k": 15, "l": 15, "m": 15, "n": 15, "o": 15, "p": 15, "q": 15, "r": 15, "s": 15, "t": 15, "u": 15, "v": 15, "w": 15, "x": 15, "y": 15, "z": 15, "\"": 16}, // 15
+  {"a": 15, "b": 15, "c": 15, "d": 15, "e": 15, "f": 15, "g": 15, "h": 15, "i": 15, "j": 15, "k": 15, "l": 15, "m": 15, "n": 15, "o": 15, "p": 15, "q": 15, "r": 15, "s": 15, "t": 15, "u": 15, "v": 15, "w": 15, "x": 15, "y": 15, "z": 15, " ": 15, "\"": 16}, // 15
   {"accept": "T_string"}, // 16
   {"=": 47}, // 17
   {"accept": "T_plus"}, // 18
@@ -24,7 +24,7 @@ var tokenMatrix =
   {"a": 23}, // 22
   {"n": 24}, // 23
   {"accept": "T_type"}, // 24
-  {"accept": "T_id", "a": 26}, // 25
+  {"accept": "T_id", "r": 48}, // 25
   {"l": 27}, // 26
   {"s": 28}, // 27
   {"e": 29}, // 28
@@ -32,7 +32,7 @@ var tokenMatrix =
   {"accept": "T_if"}, // 30
   {"t": 32}, // 31
   {"accept": "T_type"}, // 32
-  {"t": 34}, // 33
+  {"accept": "T_print"}, // 33
   {"r": 35}, // 34
   {"i": 36}, // 35
   {"n": 37}, // 36
@@ -46,5 +46,58 @@ var tokenMatrix =
   {"e": 45}, // 44
   {"accept": "T_while"}, // 45
   {"accept": "T_boolop"}, // 46
-  {"accept": "T_boolop"} // 47
+  {"accept": "T_boolop"}, // 47
+  {"i": 49}, // 48
+  {"n": 50}, // 49
+  {"t": 33} // 50
 ];
+var inputIndex;
+var lineNumber;
+var linePosition;
+var matrixPosition;
+
+function lex(){
+  inputIndex = 0;
+  lineNumber = 1;
+  linePosition = 0;
+  matrixPosition = 0;
+  var currentChar = nextChar();
+  while (currentChar != ""){
+    // found a valid state to move to
+    if (currentChar in tokenMatrix[matrixPosition]){
+      matrixPosition = tokenMatrix[matrixPosition][currentChar];
+    }
+    // whitespace found at accept state, create token
+    else if ((currentChar == " " || currentChar == "\n") && "accept" in tokenMatrix[matrixPosition]){
+      var tokenType = tokenMatrix[matrixPosition]["accept"];
+      var token = tokenType; // NYI
+      TOKENS.push(token);
+      output("Accepted token: {0}".format(tokenType));
+      matrixPosition = 0;
+    }
+    // non-terminating whitespace
+    else if ((currentChar == " " || currentChar == "\n") && matrixPosition == 0){
+      // do nothing before moving to the next character
+    }
+    // invalid character, panic
+    else{
+      output("Lex error at line {0} character {1}: unexpected character '{2}'".format(lineNumber, linePosition, currentChar));
+      return;
+    }
+    currentChar = nextChar();
+  }
+  return true;
+}
+
+function nextChar(){
+  // get the next character and advance the pointer
+  var nextChar = INPUT.charAt(inputIndex++);
+  if (nextChar == "\n"){
+    lineNumber++;
+    linePosition = 0;
+  }
+  else{
+    linePosition++;
+  }
+  return nextChar;
+}
